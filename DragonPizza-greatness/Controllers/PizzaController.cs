db96b9ed43fdab3ed06312d1c7be1aa421841b20
Everett Harris <eve1harris2638@outlook.com>
2023-03-16 01:51:46 -0400

    Added new folder and new Controller and a RAZOR pg

---
+++ DragonPizza-greatness/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DragonPizza_greatness.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
