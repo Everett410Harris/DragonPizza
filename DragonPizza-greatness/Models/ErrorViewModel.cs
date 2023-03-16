8861dded1748eea7be809b2dd287f9674c4ea83e
Everett Harris <eve1harris2638@outlook.com>
2023-03-16 01:21:15 -0400

    Add project files.

---
+++ DragonPizza-greatness/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DragonPizza_greatness.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
